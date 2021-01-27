7dedd9633dd05042c687e6121bc7cd61b594c6da
王小右 <wg_python@163.com>
2021-01-27 21:57:19 +0800

    test:casbin简单示例

---
+++ examples/demo_casbin/README.md
@@ -0,0 +1,21 @@
+# casbin 权限认证管理
+
+> 一个权限认证的库，主要是用于Go语言的，也实现了其他语言的扩展。我也是看到[奇淼](https://github.com/piexlmax)的B站关于这个的介绍，所以才接触到这个
+所以想把这引入到FastAPI中。
+
+## 安装依赖
+```
+pip install casbin
+pip install casbin_sqlalchemy_adapter
+```
+
+## 学前准备
+
+可以先在 [官方编辑器](https://casbin.org/zh-CN/editor)里面弄明白不同访问策略的原理。
+
+
+## 参考
+
+- [官网](https://casbin.org/zh-CN/)
+- [官方编辑器](https://casbin.org/zh-CN/editor)
+- [奇淼B站关于casbin介绍 基础概念理解](https://www.bilibili.com/video/BV1qz4y167XP)
---
+++ examples/demo_casbin/casbin_orm.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/1/27 20:06
+# @Author  : CoderCharm
+# @File    : casbin_orm.py
+# @Software: PyCharm
+# @Github  : github/CoderCharm
+# @Email   : wg_python@163.com
+# @Desc    :
+"""
+https://github.com/pycasbin/sqlalchemy-adapter/blob/master/README.md
+
+pip install casbin_sqlalchemy_adapter
+
+"""
+
+import casbin
+import casbin_sqlalchemy_adapter
+
+adapter = casbin_sqlalchemy_adapter.Adapter('sqlite:///test.db')
+
+e = casbin.Enforcer('./model.conf', adapter, True)
+
+sub = "nick"  # the user that wants to access a resource.
+obj = "data1"  # the resource that is going to be accessed.
+act = "read"  # the operation that the user performs on the resource.
+
+# 添加
+# e.add_policy("alice", "data1", "read")
+
+if e.enforce(sub, obj, act):
+    # permit alice to read data1casbin_sqlalchemy_adapter
+    print("通过")
+else:
+    # deny the request, show an error
+    print("拒绝")
+
