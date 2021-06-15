13a7e98cd663abbbaf157d22375d15d6269eebbb
王小右 <wg_python@163.com>
2021-06-15 08:56:23 +0800

    Update sys_casbin.py
    fix: casbin #17

--- {{cookiecutter.project_name}}/common/sys_casbin.py
+++ {{cookiecutter.project_name}}/common/sys_casbin.py
@@ -24,7 +24,7 @@ from core.config import settings
 def get_casbin() -> casbin.Enforcer:
     adapter = casbin_sqlalchemy_adapter.Adapter(engine)
 
-    e = casbin.Enforcer(settings.CASBIN_MODEL_PATH, adapter, True)
+    e = casbin.Enforcer(settings.CASBIN_MODEL_PATH, adapter)
     # e.add_function("ParamsMatch", params_match_func)
 
     return e
